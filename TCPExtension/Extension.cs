5cef1a56bee219f637e961858856791e88526bc4
Akram-Boktor <35446384+AkramBoktor@users.noreply.github.com>
2021-04-01 13:05:03 +0200

    Add files via upload

---
+++ TCPExtension/Extension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TCPExtension
+{
+    public static class Extension
+    {
+        
+        public static List<T> filter<T>(this List<T> records , Func<T,bool> func)
+        {
+            List<T> filterList = new List<T>();
+            foreach (T record in records)
+            {
+                if (func(record))
+                {
+                    filterList.Add(record);
+                }
+            }
+
+            return filterList; 
+        }
+    }
+}
